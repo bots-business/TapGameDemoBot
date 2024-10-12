251ba04426bb2ed66518e660826a57dd561d614c
Bots.Business <git.sync@bots.business>
2024-10-12 16:58:08 +0000

    Bots.Business export for bot id: 1487973

---
+++ commands/🐛 Testing/testScript.js
@@ -0,0 +1,17 @@
+/*CMD
+  command: testScript
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: 🐛 Testing
+  answer: 
+  keyboard: 
+  aliases: 
+  group: 
+CMD*/
+
+// test.js file with automatic testing
+WebApp.render({
+  template: "test.js",
+  mime_type: "application/javascript"
+});
